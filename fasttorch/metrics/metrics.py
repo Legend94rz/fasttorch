4ee984aa2e95fe331bcc16a7e4bef4cd58de8f45
renzhen <renzhen@kuaishou.com>
2021-03-08 19:10:45 +0800

    mse mae

--- fasttorch/metrics/metrics.py
+++ fasttorch/metrics/metrics.py
@@ -1,7 +1,8 @@
 from sklearn.metrics import roc_auc_score
+import torch as T
 
 
-def binary_accuracy(input, target):
+def binary_accuracy(input: T.Tensor, target: T.Tensor):
     return (input.float().round() == target).float().mean().cpu().numpy()
 
 
@@ -15,3 +16,11 @@ def categorical_accuracy(input, target):
 
 def auc_score(input, target, **kwargs):
     return roc_auc_score(target.cpu().numpy(), input.cpu().numpy(), **kwargs)
+
+
+def mean_squared_error(input: T.Tensor, target: T.Tensor):
+    return ((input - target)**2).mean()
+
+
+def mean_absolute_error(y_true, y_pred):
+    return (y_pred - y_true).abs().mean()
